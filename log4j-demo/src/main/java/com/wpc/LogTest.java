2f091d2ee9bc38f6872167fc353776867618826a
peican996 <17756311926@163.com>
2022-03-24 23:13:58 +0800

    [修改人]wpc [修改内容]新增log4j-demo初始化

--- log4j-demo/src/main/java/com/wpc/LogTest.java
+++ log4j-demo/src/main/java/com/wpc/LogTest.java
@@ -1,9 +1,18 @@
 package com.wpc;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+
 /**
  * @author wangpeican
  * @date 2022/3/24 22:32
  */
 public class LogTest {
+    private static Logger logger = LogManager.getLogger(LogTest.class.getName());
 
+    @Test
+    public void testLog4j2() {
+        logger.info("info");
+    }
 }
